302180a1c3b7b567319591175fbf6f0ac1f03530
KappyJS <45543401+KappyJS@users.noreply.github.com>
2018-12-27 16:15:05 +0300

    Update connection.js

--- src/bd/connection.js
+++ src/bd/connection.js
@@ -1,10 +1,11 @@
 const { Client } = require('pg');
 const client = new Client({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'App',
-    password: '',
+    user: 'whoigvgbozbtli',
+    host: 'ec2-46-137-99-175.eu-west-1.compute.amazonaws.com',
+    database: 'd6kqrlrip5n24n',
+    password: '701568d08b10dcf8daded3e9b6a639fcb096b302dd90324722ad9666edaaaf79',
     port: 5432,
+    ssl:true 
 })
 
 client.connect((err) => {
